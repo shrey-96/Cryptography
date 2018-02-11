5de3e787b7503e712bfd44848abd026105dd3350
Shrey <tiwarishreyansh15@rediffmail.com>
2018-02-11 02:06:12 -0500

    Working chat application

--- Cryptography/Form1.Designer.cs
+++ Cryptography/Form1.Designer.cs
@@ -28,12 +28,325 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.ServerOrClient = new System.Windows.Forms.Panel();
+            this.RunClient = new System.Windows.Forms.Button();
+            this.RunServer = new System.Windows.Forms.Button();
+            this.StartServerPanel = new System.Windows.Forms.Panel();
+            this.StartServer = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ServerPort = new System.Windows.Forms.RichTextBox();
+            this.StartClientPanel = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ipbox = new System.Windows.Forms.RichTextBox();
+            this.StartClient = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clientport = new System.Windows.Forms.RichTextBox();
+            this.ChatPanel = new System.Windows.Forms.Panel();
+            this.ConnectionInfo = new System.Windows.Forms.RichTextBox();
+            this.SendEncrypted = new System.Windows.Forms.Button();
+            this.TypeBox = new System.Windows.Forms.RichTextBox();
+            this.SendNormal = new System.Windows.Forms.Button();
+            this.ChatHistory = new System.Windows.Forms.ListBox();
+            this.WaitingPanel = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ServerOrClient.SuspendLayout();
+            this.StartServerPanel.SuspendLayout();
+            this.StartClientPanel.SuspendLayout();
+            this.ChatPanel.SuspendLayout();
+            this.WaitingPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ServerOrClient
+            // 
+            this.ServerOrClient.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ServerOrClient.BackgroundImage")));
+            this.ServerOrClient.Controls.Add(this.RunClient);
+            this.ServerOrClient.Controls.Add(this.RunServer);
+            this.ServerOrClient.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ServerOrClient.Location = new System.Drawing.Point(0, 0);
+            this.ServerOrClient.Name = "ServerOrClient";
+            this.ServerOrClient.Size = new System.Drawing.Size(874, 523);
+            this.ServerOrClient.TabIndex = 0;
+            // 
+            // RunClient
+            // 
+            this.RunClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RunClient.Image = ((System.Drawing.Image)(resources.GetObject("RunClient.Image")));
+            this.RunClient.Location = new System.Drawing.Point(321, 255);
+            this.RunClient.Name = "RunClient";
+            this.RunClient.Size = new System.Drawing.Size(211, 62);
+            this.RunClient.TabIndex = 1;
+            this.RunClient.Text = "Run Client";
+            this.RunClient.UseVisualStyleBackColor = true;
+            this.RunClient.Click += new System.EventHandler(this.RunClient_Click);
+            // 
+            // RunServer
+            // 
+            this.RunServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RunServer.Image = ((System.Drawing.Image)(resources.GetObject("RunServer.Image")));
+            this.RunServer.Location = new System.Drawing.Point(321, 154);
+            this.RunServer.Name = "RunServer";
+            this.RunServer.Size = new System.Drawing.Size(211, 63);
+            this.RunServer.TabIndex = 0;
+            this.RunServer.Text = "Run Server";
+            this.RunServer.UseVisualStyleBackColor = true;
+            this.RunServer.Click += new System.EventHandler(this.RunServer_Click);
+            // 
+            // StartServerPanel
+            // 
+            this.StartServerPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("StartServerPanel.BackgroundImage")));
+            this.StartServerPanel.Controls.Add(this.StartServer);
+            this.StartServerPanel.Controls.Add(this.label1);
+            this.StartServerPanel.Controls.Add(this.ServerPort);
+            this.StartServerPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StartServerPanel.Location = new System.Drawing.Point(0, 0);
+            this.StartServerPanel.Name = "StartServerPanel";
+            this.StartServerPanel.Size = new System.Drawing.Size(874, 523);
+            this.StartServerPanel.TabIndex = 2;
+            // 
+            // StartServer
+            // 
+            this.StartServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartServer.Image = ((System.Drawing.Image)(resources.GetObject("StartServer.Image")));
+            this.StartServer.Location = new System.Drawing.Point(276, 268);
+            this.StartServer.Name = "StartServer";
+            this.StartServer.Size = new System.Drawing.Size(276, 49);
+            this.StartServer.TabIndex = 2;
+            this.StartServer.Text = "Start Server";
+            this.StartServer.UseVisualStyleBackColor = true;
+            this.StartServer.Click += new System.EventHandler(this.StartServer_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Image = ((System.Drawing.Image)(resources.GetObject("label1.Image")));
+            this.label1.Location = new System.Drawing.Point(276, 176);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port:";
+            // 
+            // ServerPort
+            // 
+            this.ServerPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ServerPort.Location = new System.Drawing.Point(407, 173);
+            this.ServerPort.Name = "ServerPort";
+            this.ServerPort.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.ServerPort.Size = new System.Drawing.Size(145, 42);
+            this.ServerPort.TabIndex = 0;
+            this.ServerPort.Text = "23";
+            this.ServerPort.KeyDown += new System.Windows.Forms.KeyEventHandler(this.EnterPressedServerPort);
+            // 
+            // StartClientPanel
+            // 
+            this.StartClientPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("StartClientPanel.BackgroundImage")));
+            this.StartClientPanel.Controls.Add(this.label3);
+            this.StartClientPanel.Controls.Add(this.ipbox);
+            this.StartClientPanel.Controls.Add(this.StartClient);
+            this.StartClientPanel.Controls.Add(this.label2);
+            this.StartClientPanel.Controls.Add(this.clientport);
+            this.StartClientPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.StartClientPanel.Location = new System.Drawing.Point(0, 0);
+            this.StartClientPanel.Name = "StartClientPanel";
+            this.StartClientPanel.Size = new System.Drawing.Size(874, 523);
+            this.StartClientPanel.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Image = ((System.Drawing.Image)(resources.GetObject("label3.Image")));
+            this.label3.Location = new System.Drawing.Point(208, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(147, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "IP Address:";
+            // 
+            // ipbox
+            // 
+            this.ipbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ipbox.Location = new System.Drawing.Point(413, 119);
+            this.ipbox.Name = "ipbox";
+            this.ipbox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.ipbox.Size = new System.Drawing.Size(227, 29);
+            this.ipbox.TabIndex = 2;
+            this.ipbox.Text = "127.0.0.1";
+            // 
+            // StartClient
+            // 
+            this.StartClient.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartClient.Image = ((System.Drawing.Image)(resources.GetObject("StartClient.Image")));
+            this.StartClient.Location = new System.Drawing.Point(295, 281);
+            this.StartClient.Name = "StartClient";
+            this.StartClient.Size = new System.Drawing.Size(276, 49);
+            this.StartClient.TabIndex = 4;
+            this.StartClient.Text = "Connect to Server";
+            this.StartClient.UseVisualStyleBackColor = true;
+            this.StartClient.Click += new System.EventHandler(this.StartClient_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Image = ((System.Drawing.Image)(resources.GetObject("label2.Image")));
+            this.label2.Location = new System.Drawing.Point(208, 189);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // clientport
+            // 
+            this.clientport.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientport.Location = new System.Drawing.Point(413, 186);
+            this.clientport.Name = "clientport";
+            this.clientport.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.clientport.Size = new System.Drawing.Size(227, 32);
+            this.clientport.TabIndex = 3;
+            this.clientport.Text = "23";
+            // 
+            // ChatPanel
+            // 
+            this.ChatPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ChatPanel.BackgroundImage")));
+            this.ChatPanel.Controls.Add(this.ConnectionInfo);
+            this.ChatPanel.Controls.Add(this.SendEncrypted);
+            this.ChatPanel.Controls.Add(this.TypeBox);
+            this.ChatPanel.Controls.Add(this.SendNormal);
+            this.ChatPanel.Controls.Add(this.ChatHistory);
+            this.ChatPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ChatPanel.Location = new System.Drawing.Point(0, 0);
+            this.ChatPanel.Name = "ChatPanel";
+            this.ChatPanel.Size = new System.Drawing.Size(874, 523);
+            this.ChatPanel.TabIndex = 5;
+            // 
+            // ConnectionInfo
+            // 
+            this.ConnectionInfo.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.ConnectionInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ConnectionInfo.Location = new System.Drawing.Point(3, 3);
+            this.ConnectionInfo.Name = "ConnectionInfo";
+            this.ConnectionInfo.Size = new System.Drawing.Size(698, 31);
+            this.ConnectionInfo.TabIndex = 9;
+            this.ConnectionInfo.Text = "";
+            // 
+            // SendEncrypted
+            // 
+            this.SendEncrypted.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendEncrypted.Image = ((System.Drawing.Image)(resources.GetObject("SendEncrypted.Image")));
+            this.SendEncrypted.Location = new System.Drawing.Point(681, 407);
+            this.SendEncrypted.Name = "SendEncrypted";
+            this.SendEncrypted.Size = new System.Drawing.Size(190, 49);
+            this.SendEncrypted.TabIndex = 8;
+            this.SendEncrypted.Text = "Send Encrypted";
+            this.SendEncrypted.UseVisualStyleBackColor = true;
+            // 
+            // TypeBox
+            // 
+            this.TypeBox.AcceptsTab = true;
+            this.TypeBox.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.TypeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TypeBox.Location = new System.Drawing.Point(3, 402);
+            this.TypeBox.Name = "TypeBox";
+            this.TypeBox.Size = new System.Drawing.Size(672, 118);
+            this.TypeBox.TabIndex = 6;
+            this.TypeBox.Text = "";
+            // 
+            // SendNormal
+            // 
+            this.SendNormal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SendNormal.Image = ((System.Drawing.Image)(resources.GetObject("SendNormal.Image")));
+            this.SendNormal.Location = new System.Drawing.Point(681, 462);
+            this.SendNormal.Name = "SendNormal";
+            this.SendNormal.Size = new System.Drawing.Size(190, 49);
+            this.SendNormal.TabIndex = 5;
+            this.SendNormal.Text = "Send Normal";
+            this.SendNormal.UseVisualStyleBackColor = true;
+            this.SendNormal.Click += new System.EventHandler(this.SendNormal_Click);
+            // 
+            // ChatHistory
+            // 
+            this.ChatHistory.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ChatHistory.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChatHistory.FormattingEnabled = true;
+            this.ChatHistory.ItemHeight = 29;
+            this.ChatHistory.Location = new System.Drawing.Point(3, 40);
+            this.ChatHistory.Name = "ChatHistory";
+            this.ChatHistory.Size = new System.Drawing.Size(868, 381);
+            this.ChatHistory.TabIndex = 3;
+            // 
+            // WaitingPanel
+            // 
+            this.WaitingPanel.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("WaitingPanel.BackgroundImage")));
+            this.WaitingPanel.Controls.Add(this.label4);
+            this.WaitingPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.WaitingPanel.Location = new System.Drawing.Point(0, 0);
+            this.WaitingPanel.Name = "WaitingPanel";
+            this.WaitingPanel.Size = new System.Drawing.Size(874, 523);
+            this.WaitingPanel.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Image = ((System.Drawing.Image)(resources.GetObject("label4.Image")));
+            this.label4.Location = new System.Drawing.Point(205, 221);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(476, 46);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Waiting for connection...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 523);
+            this.Controls.Add(this.ServerOrClient);
+            this.Controls.Add(this.ChatPanel);
+            this.Controls.Add(this.StartClientPanel);
+            this.Controls.Add(this.WaitingPanel);
+            this.Controls.Add(this.StartServerPanel);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ServerOrClient.ResumeLayout(false);
+            this.StartServerPanel.ResumeLayout(false);
+            this.StartServerPanel.PerformLayout();
+            this.StartClientPanel.ResumeLayout(false);
+            this.StartClientPanel.PerformLayout();
+            this.ChatPanel.ResumeLayout(false);
+            this.WaitingPanel.ResumeLayout(false);
+            this.WaitingPanel.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel ServerOrClient;
+        private System.Windows.Forms.Button RunClient;
+        private System.Windows.Forms.Button RunServer;
+        private System.Windows.Forms.Panel StartServerPanel;
+        private System.Windows.Forms.RichTextBox ServerPort;
+        private System.Windows.Forms.Button StartServer;
+        internal System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel StartClientPanel;
+        internal System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox ipbox;
+        private System.Windows.Forms.Button StartClient;
+        internal System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox clientport;
+        private System.Windows.Forms.Panel ChatPanel;
+        private System.Windows.Forms.Button SendEncrypted;
+        private System.Windows.Forms.RichTextBox TypeBox;
+        private System.Windows.Forms.Button SendNormal;
+        private System.Windows.Forms.ListBox ChatHistory;
+        private System.Windows.Forms.RichTextBox ConnectionInfo;
+        private System.Windows.Forms.Panel WaitingPanel;
+        internal System.Windows.Forms.Label label4;
     }
 }
 
--- Cryptography/Form1.cs
+++ Cryptography/Form1.cs
@@ -4,17 +4,198 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
 
 namespace Cryptography
 {
     public partial class Form1 : Form
     {
+        static TcpClient client;
+        static TcpClient RunTheClient;
+        static TcpListener server;
+        static bool ServerItIs = false;
+        private static object Dispatcher;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // go to start server page
+        private void RunServer_Click(object sender, EventArgs e)
+        {
+            StartServerPanel.BringToFront();
+        }
+
+        // go to start client page
+        private void RunClient_Click(object sender, EventArgs e)
+        {
+            StartClientPanel.BringToFront();
+        }
+
+        // start the server
+        private void StartServer_Click(object sender, EventArgs e)
+        {
+            ServerItIs = true;
+            string portString = ServerPort.Text;
+            ServerPort.Clear();
+            bool valid = false;
+
+            int temp = 0;
+            int.TryParse(portString, out temp);
+
+            if (temp > 0)
+                valid = true;
+
+            if (valid)
+            {
+                Int32 port = Int32.Parse(portString);
+                IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+                server = new TcpListener(ip, port);
+
+                // start server
+                server.Start();
+
+                // bring chat panel to front
+                ChatPanel.BringToFront();
+                ConnectionInfo.Text = "IP: " + GetLocalIPAddress() + "   Port: " + port;
+
+                ThreadPool.QueueUserWorkItem(HandleClient);
+                    
+
+            }
+        }
+
+        private void HandleClient(object o)
+        {
+            // wait for client to join
+            client = server.AcceptTcpClient();
+            StreamReader sr = new StreamReader(client.GetStream());
+
+            string msg = "";
+           
+            while (true)
+            {
+                msg = sr.ReadLine();
+                ChatHistory.Items.Add("Client: " + msg);
+
+                if (msg.Contains("shutdown"))
+                {
+                    MessageBox.Show("Breaking out now");
+                    break;
+                }
+            }
+        }
+
+        // start the server
+        private void EnterPressedServerPort(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                StartServer_Click(sender, e);
+
+        }
+
+      
+
+        private void SendNormal_Click(object sender, EventArgs e)
+        {
+            string msg = TypeBox.Text;
+            TypeBox.Clear();
+            StreamWriter sw;
+
+            if (ServerItIs)
+                sw = new StreamWriter(client.GetStream());
+            else
+                sw = new StreamWriter(RunTheClient.GetStream());
+
+            sw.AutoFlush = true;
+
+            sw.WriteLine(msg);
+            ChatHistory.Items.Add("Me: " + msg);
+        }
+
+
+        private void StartClient_Click(object sender, EventArgs e)
+        {
+            bool valid = false;
+            IPAddress ip;
+            int port = 0;            
+
+            if (IPAddress.TryParse(ipbox.Text, out ip) && int.TryParse(clientport.Text, out port))
+            {
+                if (port > 0)
+                    valid = true;
+            }
+
+            if(valid)
+            {
+                RunTheClient = new TcpClient();
+
+                try
+                {
+                    // connect to server
+                    RunTheClient.Connect(ip, port);              
+
+                    if (RunTheClient.Connected)
+                    {
+                       // MessageBox.Show("Bringing clint chat to front");
+                        ChatPanel.BringToFront();
+                        ThreadPool.QueueUserWorkItem(HandleServer);
+                    }
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+
+        }
+
+        public void HandleServer(object o)
+        {
+            string msg = "";
+
+            try
+            {
+                StreamReader sr = new StreamReader(RunTheClient.GetStream());
+
+                while (true)
+                {
+                    msg = sr.ReadLine();
+                    if (msg.Contains("shutdown"))
+                        break;
+
+                      ChatHistory.Items.Add("Server: " + msg);
+                    //MessageBox.Show("Server: " + msg);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+            }
+        }
+
+
+        // stackoverflow: get ip address
+        public static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
